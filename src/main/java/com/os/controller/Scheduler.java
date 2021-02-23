638ccb20ccd63ad9d31784a5ca0c37db5fb290ee
davea <dave.avid@gmail.com>
2021-02-23 17:54:22 -0600

    DEATH TO EVERYTHING

--- src/main/java/com/os/controller/Scheduler.java
+++ src/main/java/com/os/controller/Scheduler.java
@@ -2,8 +2,10 @@ package com.os.controller;
 
 
 import com.os.CircularLinkedList;
+import com.os.models.Process;
 
 public class Scheduler {
+
     private CircularLinkedList<Process> readyQueue;
 
     public Scheduler(){
