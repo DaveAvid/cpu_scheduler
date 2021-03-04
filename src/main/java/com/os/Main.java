d4fcc01f7b6e82375a39d88aceaf674daf17d8d9
davea <dave.avid@gmail.com>
2021-03-03 23:15:23 -0600

    Added logic to SJF, bug in IO processing

--- src/main/java/com/os/Main.java
+++ src/main/java/com/os/Main.java
@@ -1,13 +1,15 @@
 package com.os;
 
 import com.os.algorithms.FirstComeFirstServedScheduler;
+import com.os.algorithms.ShortJobFirstScheduler;
 import com.os.controller.Scheduler;
 import com.os.models.SystemProcess;
 
 public class Main {
     public static void main(String[] args) {
         //create one instance of scheduler
-        Scheduler scheduler = new FirstComeFirstServedScheduler();
+        Scheduler firstComeFirstServedScheduler = new FirstComeFirstServedScheduler();
+        Scheduler shortJobFirstScheduler = new ShortJobFirstScheduler();
 
         //create new process from file(TOODO), currently hardcoded
         {
@@ -15,19 +17,19 @@ public class Main {
             int[] ioBurst = new int[]{4, 2};
             SystemProcess systemProcess = new SystemProcess("apache", 3, 2, cpuBurst, ioBurst);
             //add process to scheduler
-            scheduler.addProcess(systemProcess);
+            shortJobFirstScheduler.addProcess(systemProcess);
         }
         {
             int[] cpuBurst = new int[]{6, 7, 10};
             int[] ioBurst = new int[]{5, 3};
             SystemProcess systemProcess = new SystemProcess("french", 2, 1, cpuBurst, ioBurst);
             //add process to scheduler
-            scheduler.addProcess(systemProcess);
+            shortJobFirstScheduler.addProcess(systemProcess);
         }
 
 
         //start scheduler thread
-        Thread schedulerThread = new Thread(scheduler);
+        Thread schedulerThread = new Thread(shortJobFirstScheduler);
         schedulerThread.start();
 
     }
--- src/main/java/com/os/algorithms/ShortJobFirstScheduler.java
+++ src/main/java/com/os/algorithms/ShortJobFirstScheduler.java
@@ -1,29 +1,93 @@
 package com.os.algorithms;
 
 import com.os.controller.Scheduler;
+import com.os.models.State;
 import com.os.models.SystemProcess;
 
 public class ShortJobFirstScheduler extends Scheduler {
+
     public void run() {
         IS_RUNNING = true;
         while (IS_RUNNING) {
+            try {
+                System.out.println("Current System Time: " + runningTime);
 
+                if (cpuCurrentProcess == null) {
+                    cpuCurrentProcess = getNextProcess();
+                    if (cpuCurrentProcess == null) {
+                        continue;
+                    }
+                }
+                if (ioCurrentProcess == null) {
+                    ioCurrentProcess = getNextIoProcess();
+                }
+
+                if (cpuCurrentProcess.cpuHasBurstRemaining()) {
+                    cpuCurrentProcess.decrementCpuBurstTime();
+                }
+                if (ioCurrentProcess != null && ioCurrentProcess.ioHasBurstRemaining()) {
+                    ioCurrentProcess.decrementIoBurst();
+                }
+                if (cpuCurrentProcess.cpuHasBurstRemaining() == false && cpuCurrentProcess.ioHasBurstRemaining() == true) {
+                    moveCurrentProcessToIoWaitQueue();
+
+                }
+                if (ioCurrentProcess != null && ioCurrentProcess.cpuHasBurstRemaining() == true && ioCurrentProcess.ioHasBurstRemaining() == false) {
+                    moveCurrentProcessToReadyQueue();
+
+                }
+                terminateIfCpuComplete();
+                terminateIfIoComplete();
+
+            } finally {
+                runningTime++;
+            }
         }
+    }
+//add more processes
+    //confirm by hand
+
+    private void moveCurrentProcessToIoWaitQueue() {
+        //set first cpu burst to cpuburstremaining
+        if (cpuCurrentProcess.getCpuBurstRemaining() == 0 && !cpuCurrentProcess.getCpuBurstQueue().isEmpty()) {
+            cpuCurrentProcess.setCpuBurstRemaining(cpuCurrentProcess.getCpuBurstQueue().remove(0));
+        }
+        ioWaitQueue.add(cpuCurrentProcess);
+        cpuCurrentProcess = null;
+    }
+
+    private void moveCurrentProcessToReadyQueue() {
+        //set first io to ioburstremaining
+        if (ioCurrentProcess.getIoBurstRemaining() == 0 && !ioCurrentProcess.getIoBurstQueue().isEmpty()) {
+            ioCurrentProcess.setIoBurstRemaining(ioCurrentProcess.getIoBurstQueue().remove(0));
+        }
+        readyQueue.add(ioCurrentProcess);
+        ioCurrentProcess = null;
+    }
+
+    private SystemProcess getNextIoProcess() {
+        SystemProcess foundProcess = ioWaitQueue.poll();
+        return foundProcess;
     }
 
     public SystemProcess getNextProcess() {
         SystemProcess foundProcess = null;
-        //iterate through list of processes
+        //iterate through queue of processes
         for (SystemProcess process : readyQueue) {
             //create a base case, set found process to process so we have something to compare to
             if (foundProcess == null) {
                 foundProcess = process;
                 //find the shortest priority level and perform priorty scheduling on the found process
-            } else if (process.getNextCpuBurst() < foundProcess.getNextCpuBurst()) {
+            } else if (process.getNextCpuBurst() > foundProcess.getNextCpuBurst()) {
                 foundProcess = process;
             }
-
         }
+        if (foundProcess != null) {
+            foundProcess.setState(State.RUNNING);
+            //now that we found next process, remove from ready queue.
+            readyQueue.remove(foundProcess);
+        }
+
         return foundProcess;
     }
 }
